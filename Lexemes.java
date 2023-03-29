44d7b40455841bd4134db679064cb6100661bf6c
LeavesEatingCow <69094343+LeavesEatingCow@users.noreply.github.com>
2023-03-28 23:15:14 -0400

    Add files via upload

--- Lexemes.java
+++ Lexemes.java
@@ -1,6 +1,11 @@
-import javax.swing.plaf.nimbus.State;
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
 
 public class Lexemes {
 
